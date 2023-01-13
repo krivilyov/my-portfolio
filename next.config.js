73bf64710538a5d7b84f9a94c2e940b837f4ddd3
Ivan Krivilyov <gramprogramming@gamil.com>
2023-01-13 10:47:53 +0500

    next start

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	experimental: {
+		appDir: true,
+	},
+};
 
-module.exports = nextConfig
+const path = require("path");
+
+module.exports = {
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+		prependData: `@import "break_points.scss";`,
+	},
+};
+
+module.exports = nextConfig;
